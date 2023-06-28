bdd4b393be5a97da114a40db1cc0bc1222c4d76f
JohnLDev <john.lenon.dev@gmail.com>
2023-06-28 11:57:43 -0300

    feat: first commit

---
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -0,0 +1,99 @@
+import Order from '../../../../domain/checkout/entity/order';
+import OrderItem from '../../../../domain/checkout/entity/order_item';
+import OrderRepositoryInterface from '../../../../domain/checkout/repository/order-repository.interface';
+import OrderItemModel from './order-item.model';
+import OrderModel from './order.model';
+
+export default class OrderRepository implements OrderRepositoryInterface {
+  async create(entity: Order): Promise<void> {
+    await OrderModel.create(
+      {
+        id: entity.id,
+        customer_id: entity.customerId,
+        total: entity.total(),
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.productId,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        include: [{ model: OrderItemModel }],
+      }
+    );
+  }
+
+  async find(id: string): Promise<Order> {
+    const order = await OrderModel.findByPk(id, {
+      include: [{ model: OrderItemModel }],
+    });
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    return new Order(
+      order.id,
+      order.customer_id,
+      order.items.map(
+        (item) =>
+          new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.product_id,
+            item.quantity
+          )
+      )
+    );
+  }
+
+  async update(entity: Order): Promise<void> {
+    if (entity.items.length === 0) {
+      throw new Error('Order must have at least one item');
+    }
+    for (const item of entity.items) {
+      await OrderItemModel.upsert({
+        id: item.id,
+        name: item.name,
+        price: item.price,
+        product_id: item.productId,
+        quantity: item.quantity,
+        order_id: entity.id,
+      });
+    }
+    await OrderModel.update(
+      {
+        total: entity.total(),
+      },
+      {
+        where: { id: entity.id },
+      }
+    );
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orders = await OrderModel.findAll({
+      include: [{ model: OrderItemModel }],
+    });
+
+    return orders.map(
+      (order) =>
+        new Order(
+          order.id,
+          order.customer_id,
+          order.items.map(
+            (item) =>
+              new OrderItem(
+                item.id,
+                item.name,
+                item.price,
+                item.product_id,
+                item.quantity
+              )
+          )
+        )
+    );
+  }
+}
